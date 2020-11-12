a53dcf971903eaa74e9031e214661550e63c6e11
Zac Zidik <zez1@psu.edu>
2020-11-12 09:24:27 -0500

    Test

---
+++ PuppetJump/Scripts/Utils/HandDisplayObject.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PuppetJump.Utils
+{
+    public class HandDisplayObject : MonoBehaviour
+    {
+        public RuntimeAnimatorController oculusController;
+        public RuntimeAnimatorController viveController;
+        public Animator animator;
+
+        private void Update()
+        {
+            if (PuppetJumpManager.Instance.vrReady && animator.runtimeAnimatorController == null)
+            {
+                switch (PuppetJumpManager.Instance.deviceType)
+                {
+                    case PuppetJumpManager.DeviceTypes.OVR:
+                        animator.runtimeAnimatorController = oculusController;
+                        break;
+                    case PuppetJumpManager.DeviceTypes.SteamVR:
+                        animator.runtimeAnimatorController = viveController;
+                        break;
+                }
+            }
+        }
+
+        public void TriggerButton(bool down)
+        {
+            animator.SetBool("trigger", down);
+        }
+
+        public void GripButton(bool down)
+        {
+            animator.SetBool("grip", down);
+        }
+
+        public void ButtonOne(bool down)
+        {
+            animator.SetBool("buttonOne", down);
+        }
+
+        public void ButtonTwo(bool down)
+        {
+            animator.SetBool("buttonTwo", down);
+        }
+
+        public void Joystick(bool down)
+        {
+            animator.SetBool("joystick", down);
+        }
+    }
+}
